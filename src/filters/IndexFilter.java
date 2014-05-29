076a78e761c03a3beb69b38eb4fd4fa48167b32f
ZeRenato <renatoandradej@gmail.com>
2014-05-28 23:41:35 -0300

    Substituição de um Servlet para um Filter em index
    -Exclusão do IndexServlet
    -Inclusão do IndexFilter
    -Ajuste no mapeamento no web.xml

---
+++ src/filters/IndexFilter.java
@@ -0,0 +1,58 @@
+package filters;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import classes.GerenciadorProduto;
+import classes.Produto;
+
+/**
+ * Servlet Filter implementation class IndexFilter
+ */
+@WebFilter("/IndexFilter")
+public class IndexFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public IndexFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		GerenciadorProduto gp = new GerenciadorProduto();
+    	ArrayList<Produto> produtos = new ArrayList<Produto>();
+    	for (Produto p : gp.getProdutos()) {
+			if (p.isDestaque())
+				produtos.add(p);
+		}
+    	request.setAttribute("produtos", produtos);
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}

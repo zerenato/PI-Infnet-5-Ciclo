79a0de382b0af83dc3401e455e84a2c0116b880b
ZeRenato <renatoandradej@gmail.com>
2014-06-01 23:56:01 -0300

    Ajuste no filtro de Autenticação

--- src/filters/CheckoutFilter.java
+++ src/filters/CheckoutFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class AutenticacaoFilter implements Filter {
+public class CheckoutFilter implements Filter {
 
     private String loginUrl = null;
     private String loginPath = null;
